2ee68c08401eda274c622466d7711920257e6868
AidanBlackwell <aidanblackwell58367@gmail.com>
2025-09-16 12:24:54 -0500

    Add files via upload
    part 2 added

---
+++ #Part 2.py
@@ -0,0 +1,12 @@
+#Part 2
+
+#Marcus is frozen in fear
+#Panicked, he tries to shut down his computer
+
+def part2():
+    print("Marcus attempts to close all of his applications, but nothing is of use.")
+    print("Another message appears, 'There's nothing you can do now, Marcus'")
+    print("A new file is created, Marcus hesitantly opens it up")
+
+part2()
+
