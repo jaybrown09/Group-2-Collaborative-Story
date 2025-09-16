5db98382781643b4aa938859389e4a4c82f02c35
Brogers11 <162910180+Brogers11@users.noreply.github.com>
2025-09-16 12:31:57 -0500

    Add files via upload

---
+++ BraydenRogers_Part3.py
@@ -0,0 +1,13 @@
+# Brayden Rogers Part 3
+# 09/16/2025
+
+# Marcus recalled what he had been told over and over in cyber safety videos
+# did not open the file.
+# Instead, he deleted it and rebooted his computer.
+
+def story3():
+    print("He the shut down his computer, figured it must have been a virus.")
+    print("He knew one day all the pirated movies and games would catch up to him.")
+    print("After shutting down his computer, he got up, calmed his nerves, and went to bed.")
+
+story3()
