eb8a9fbf3ec4b5da19ef861358f92c68fec2ae4c
Tlyddell <tyddell@gmail.com>
2025-09-18 11:18:13 -0500

    Part 4

---
+++ Part 4 Collabrotive Story.py
@@ -0,0 +1,14 @@
+#Part 4
+
+# He awakes from his rest thinking it was all a dream
+# He stumbles over to the computer to see for himself
+
+# He walks over to his laptop and takes a look
+#
+
+def part4():
+    print("Opening his laptop he only sees one thing. Its over.")
+    print("The screen again begins to fill text by itself you think it would be that easy.")
+    print("You cannot keep getting away with this the computer types it out Marcus says, getting away with what.")
+
+part4()
