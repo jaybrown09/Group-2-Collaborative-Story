15a4ef8001242fc8d4db5559afe16f27dbf89fc7
christian-stuart <christianstuart2003@gmail.com>
2025-09-18 12:01:43 -0500

    Add files via upload

---
+++ Part_5.py
@@ -0,0 +1,15 @@
+#Part 5
+#Christian Stuart
+
+#He looks at his laptop and reads the text
+#He feels something touching his stomach
+#he disappears
+
+def part5():
+    print("As he finishes reading the text, the screen goes blank and apair of hands appear.")
+    print("He feels something on his stomach and looks down.")
+    print("The hands from the screen reach out and grab him.")
+    print("He feels a strong pull......and is never seen again.")
+
+    part5()
+
